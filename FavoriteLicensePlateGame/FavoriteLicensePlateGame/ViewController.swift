d02b32fd3ac1903d4ef59a2494f551f7bcf05bf8
jmondale <jmondale@users.noreply.github.com>
2016-09-30 08:46:50 -0600

    Add files via upload
    Initial import of license plate game project

---
+++ FavoriteLicensePlateGame/FavoriteLicensePlateGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FavoriteLicensePlateGame
+//
+//  Created by Jaye Mondale on 9/29/16.
+//  Copyright © 2016 5mConsult. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
